60a1ad4db9a8cf8912ccdd54d76a7323b06d059b
mershov <maxx95.95@gmail.com>
2023-04-01 17:42:19 +0600

    fix: translate handle

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,22 @@ const App = () => {
     { word: 'Cuatro' },
     { word: 'Four', isTranslate: true },
   ]);
+  const [isTranslated, setIsTranslated] = useState(false);
 
   const handleSwipeRight = () => {
+    setIsTranslated(true);
     setItems((prev) => prev.slice(1));
   };
 
   const handleSwipeLeft = () => {
-    setItems((prev) => prev.slice(2));
+    setIsTranslated(false);
+
+    setItems((prev) => {
+      if (isTranslated) {
+        return prev.slice(1);
+      }
+      return prev.slice(2);
+    });
   };
 
   return (
